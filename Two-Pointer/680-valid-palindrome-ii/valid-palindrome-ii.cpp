b5d1f9af785862f388ab2a33664e672935ebfa77
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2024-08-31 10:42:59 +0530

    Time: 55 ms (50.95%) | Memory: 25.1 MB (69.64%) - LeetSync

---
+++ Two-Pointer/680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s){
+        int l = 0, r = s.length() - 1;
+
+        while(l < r){
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int l = 0, r = s.length() - 1, flag = 0;
+
+        while(l <= r){
+            if(s[l] != s[r]){
+                return isPalindrome(s.substr(l, r-l)) || isPalindrome(s.substr(l+1, r-l));
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
