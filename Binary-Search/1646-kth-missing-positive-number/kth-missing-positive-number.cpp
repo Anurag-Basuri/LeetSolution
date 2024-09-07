cfdedf4a4c9bf038ec96e7fec44ec4fbd60c3772
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2024-09-07 21:10:51 +0530

    Time: 4 ms (38.32%) | Memory: 12.1 MB (92.51%) - LeetSync

---
+++ Binary-Search/1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& nums, int k) {
+        int left = 0, right = nums.size() - 1;
+
+        if(nums[right] == nums.size()) return nums[right] + k;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int missing = nums[mid] - mid - 1;
+
+            if(missing >= k) right = mid - 1;
+            else left = mid + 1;
+
+            cout << right << " " << left;
+        }
+
+        return left + k;
+    }
+};
