a69c8b8a15bee2645786cf40767e1741d9cd2f9e
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2024-11-09 11:18:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (52.80%) - LeetSync

--- Bit_Manipulations/3394-minimum-array-end/minimum-array-end.cpp
+++ Bit_Manipulations/3394-minimum-array-end/minimum-array-end.cpp
@@ -7,8 +7,7 @@ public:
 
         while (i_n <= n - 1) {
             if ((i_x & x) == 0) {
-                if (i_n & (n - 1)) 
-                    res |= i_x;
+                res |= i_x * (i_n & (n - 1) ? 1 : 0);
 
                 i_n <<= 1;
             }
