54b783d5e42119fc28101d2cfab5dc52dee174e8
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2024-12-26 05:43:56 +0530

    Time: 52 ms (35.46%) | Memory: 66.3 MB (19.25%) - LeetSync

--- Graphs/820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ Graphs/820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, vector<int>& vis, vector<int>& ans,
-             int node) {
-        if (vis[node] == 2)
-            return true;
-
-        if(vis[node] == 1)
-            return false;
-
-        vis[node] = 1;
-
-        for (auto i : graph[node]) {
-            if (!dfs(graph, vis, ans, i))
-                return false;
-        }
-
-        vis[node] = 2;
-        ans.push_back(node);
-        return true;
-    }
-
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n, 0);
+        vector<vector<int>> ultagraph(n);
+        vector<int> indegree(n, 0);
         vector<int> ans;
 
         for (int i = 0; i < n; i++) {
-            if (vis[i] == 0)
-                dfs(graph, vis, ans, i);
+            for (auto it : graph[i]) {
+                ultagraph[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for (auto it : ultagraph[node]) {
+                indegree[it]--;
+
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
         }
 
         sort(ans.begin(), ans.end());
