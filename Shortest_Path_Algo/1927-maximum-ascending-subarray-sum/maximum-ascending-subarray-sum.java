2ca27f074ce0f354502a75a44a8318f4e64238f1
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2025-02-04 10:40:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (39.25%) - LeetSync

--- Shortest_Path_Algo/1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
+++ Shortest_Path_Algo/1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maxAscendingSum(int[] nums) {
-        int max_sum = nums[0];
+        int max_sum = 0;
         int cur_sum = nums[0];
 
         for(int i = 1; i < nums.length; i++){
+                max_sum = Math.max(max_sum, cur_sum);
             if(nums[i] > nums[i - 1]){
                 cur_sum += nums[i];
-                max_sum = Math.max(max_sum, cur_sum);
             }
             else cur_sum = nums[i];
         }
+
+        max_sum = Math.max(max_sum, cur_sum);
 
         return max_sum;
     }
