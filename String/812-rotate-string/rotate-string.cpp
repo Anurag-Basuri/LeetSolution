7c48381547e4afc0462e2ef8cddeb9a4209699d3
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2025-03-19 17:19:43 +0530

    xdgfchcfxcvbn

--- String/812-rotate-string/rotate-string.cpp
+++ String/812-rotate-string/rotate-string.cpp
@@ -1,23 +1,7 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if (s.length() != goal.length()) {
-            return false;
-        }
-        
-        if (s == goal) {
-            return true;
-        }
-        
-        int l = s.length();
-        while (l > 0) {
-            s = s.substr(1) + s[0];
-            if (s == goal) {
-                return true;
-            }
-            l--;
-        }
-        
-        return false;
+        if(s.length() != goal.length()) return false;
+        return (s + s).find(goal) != string::npos;
     }
 };
