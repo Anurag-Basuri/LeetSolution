a34f2f9f238f75a8cae4611d784fb4de9d40f31e
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2025-11-25 12:54:51 +0530

    Time: 23 ms (10.77%) | Memory: 15.8 MB (11.24%) - LeetSync

--- Arrays/1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
+++ Arrays/1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int smallestRepunitDivByK(int k) {
-        if(k%2 == 0) return -1;
-        if(k%5 == 0) return -1;
+        if(k%2 == 0 || k%5 == 0) return -1;
 
         unordered_set<int> reminders;
         int reminder = 1, count = 1;
 
         while(reminder%k != 0) {
             int n = reminder * 10 + 1;
-
             reminder = n % k;
             count++;
 
