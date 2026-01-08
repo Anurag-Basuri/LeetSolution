5f5c698d82c6ef7c0a4473bdbe32071d38e6f5c5
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-01-08 12:30:39 +0530

    Time: 29 ms (44.13%) | Memory: 22.8 MB (38.55%) - LeetSync

---
+++ Stack/3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long calculateScore(string s) {
+        long long sum = 0;
+        int n = s.size();
+        unordered_map<char, stack<int>> stMap;
+
+        for(int i = 0; i < n; i++) {
+            char ch = s[i];
+            char chMir = 'z' - (ch - 'a');
+
+            if(!stMap[chMir].empty()) {
+                sum += i - stMap[chMir].top();
+                stMap[chMir].pop();
+            } else
+                stMap[ch].push(i);
+        }
+
+        return sum;
+    }
+};
