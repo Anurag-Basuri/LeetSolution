b828d36748a9b5872c5a31a4fcc7a61f519da3ca
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-01-21 10:02:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.29%) - LeetSync

--- Dynamic_Programming/70-climbing-stairs/climbing-stairs.cpp
+++ Dynamic_Programming/70-climbing-stairs/climbing-stairs.cpp
@@ -3,13 +3,15 @@ public:
     int climbStairs(int n) {
         if (n <= 1) return n;
 
-        int dp[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        int a = 1;
+        int b = 1;
 
-        for (int i = 2; i <= n; i++)
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for (int i = 2; i <= n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
 
-        return dp[n];
+        return b;
     }
 };
