c9b53105e45969805adb6d9dc5cb51439db5152f
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-02-15 14:52:34 +0530

    Time: 41 ms (28.91%) | Memory: 61.4 MB (12.04%) - LeetSync

--- Dynamic_Programming/518-coin-change-ii/coin-change-ii.cpp
+++ Dynamic_Programming/518-coin-change-ii/coin-change-ii.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    int recur(int i, vector<int> &coins, vector<vector<int>> &dp, int amt) {
-        if(amt == 0) return 1;
-
-        if(i == 0) return 0;
-
-        if(dp[i][amt] != -1) return dp[i][amt];
-
-        int notTake = recur(i - 1, coins, dp, amt);
-
-        int take = 0;
-        if(amt >= coins[i - 1])
-            take = recur(i, coins, dp, amt - coins[i - 1]);
-
-        return dp[i][amt] = take + notTake; 
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
 
-        return recur(n, coins, dp, amount);
+        if (amount == 0) return 1;
+        if (n == 0) return 0;
+
+        vector<vector<long long>> dp(n, vector<long long>(amount + 1, 0));
+
+        // base case
+        for (int j = 0; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                __int128 notTaken = dp[i - 1][j];
+                __int128 taken = 0;
+
+                if (coins[i] <= j)
+                    taken = dp[i][j - coins[i]];
+
+                dp[i][j] = (long long)(notTaken + taken);
+            }
+        }
+
+        return (int)dp[n - 1][amount];
     }
 };
