54df95e9257e22eded5a66f4908e576d40d5c41c
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-02-19 18:47:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (70.39%) - LeetSync

--- Dynamic_Programming/62-unique-paths/unique-paths.cpp
+++ Dynamic_Programming/62-unique-paths/unique-paths.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int recur(int i, int j, vector<vector<int>> &dp) {
-        if(i == 0 && j == 0) return 1;
-
-        if(dp[i][j]) return dp[i][j];
-
-        int top = 0;
-        if(i - 1 >= 0) top = recur(i - 1, j, dp);
-
-        int left = 0;
-        if(j - 1 >= 0) left = recur(i, j - 1, dp);
-
-        return dp[i][j] = top + left;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> grid(n , vector<int>(m, 0));
+        vector<vector<int>> dp(n, vector<int>(m, 0));
 
-        return recur(n - 1, m - 1, grid);
+        for(int i = 0; i < m; i++) dp[0][i] += 1;
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                dp[i][j] = dp[i - 1][j];
+
+                if(j >= 1) dp[i][j] += dp[i][j - 1];
+            }
+        }
+
+        return dp[n - 1][m - 1];
     }
 };
