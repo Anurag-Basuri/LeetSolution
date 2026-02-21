1778393d91a872add02ef00ad8f7b49559c52049
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-02-21 11:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (23.78%) - LeetSync

--- Dynamic_Programming/120-triangle/triangle.cpp
+++ Dynamic_Programming/120-triangle/triangle.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
+    int travel(int level, int m, int index, 
+               vector<vector<int>>& triangle, 
+               vector<vector<int>>& dp) {
+        
+        // Base case: last row
+        if(level == m - 1) 
+            return triangle[level][index];
+
+        // If already computed
+        if(dp[level][index] != INT_MAX) 
+            return dp[level][index];
+        
+        // Recursive relation
+        dp[level][index] = triangle[level][index] + 
+            min(travel(level + 1, m, index, triangle, dp),
+                travel(level + 1, m, index + 1, triangle, dp));
+
+        return dp[level][index];
+    }
+
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
+        int m = triangle.size();
 
-        vector<int> dp = triangle[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j];
-            }
+        // DP initialization
+        vector<vector<int>> dp(m);
+        for(int i = 0; i < m; i++) {
+            dp[i] = vector<int>(triangle[i].size(), INT_MAX);
         }
 
-        return dp[0];
+        // Start from top
+        return travel(0, m, 0, triangle, dp);    
     }
 };
