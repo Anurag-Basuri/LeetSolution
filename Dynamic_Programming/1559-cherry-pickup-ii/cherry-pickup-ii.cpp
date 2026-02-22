b0259b182b6c77201fc59c3fc7a19552fb6d731f
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-02-22 12:23:51 +0530

    Time: 75 ms (18.75%) | Memory: 20.8 MB (81.07%) - LeetSync

--- Dynamic_Programming/1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ Dynamic_Programming/1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,47 +1,58 @@
 class Solution {
 public:
-    int f(int i, int j1, int j2, int n, int m, vector<vector<int>> &grid, vector<vector<vector<int>>> &dp) {
-        // Out of bounds
-        if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m)
-            return -1e9;
-
-        // Last row processed
-        if (i == n)
-            return 0;
-
-        // Memo check
-        if (dp[i][j1][j2] != -1)
-            return dp[i][j1][j2];
-
-        int ans = -1e9;
-
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-
-                int val;
-                if (j1 == j2)
-                    val = grid[i][j1];
-                else
-                    val = grid[i][j1] + grid[i][j2];
-
-                int next = f(i + 1, j1 + x, j2 + y, n, m, grid, dp);
-
-                ans = max(ans, val + next);
-            }
-        }
-
-        return dp[i][j1][j2] = ans;
-    }
-
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
+        // dp[i][j1][j2]
         vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(m, vector<int>(m, -1))
+            n, vector<vector<int>>(m, vector<int>(m, 0))
         );
 
-        // Robot 1 starts at (0, 0), Robot 2 at (0, m-1)
-        return f(0, 0, m - 1, n, m, grid, dp);
+        // Base case: last row
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2)
+                    dp[n-1][j1][j2] = grid[n-1][j1];
+                else
+                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+
+        // Fill from bottom to top
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+
+                    int ans = -1e9;
+
+                    for (int x = -1; x <= 1; x++) {
+                        for (int y = -1; y <= 1; y++) {
+
+                            int nj1 = j1 + x;
+                            int nj2 = j2 + y;
+
+                            if (nj1 < 0 || nj1 >= m || nj2 < 0 || nj2 >= m)
+                                continue;
+
+                            int value;
+                            if (j1 == j2)
+                                value = grid[i][j1];
+                            else
+                                value = grid[i][j1] + grid[i][j2];
+
+                            value += dp[i + 1][nj1][nj2];
+
+                            ans = max(ans, value);
+                        }
+                    }
+
+                    dp[i][j1][j2] = ans;
+                }
+            }
+        }
+
+        // Start positions
+        return dp[0][0][m - 1];
     }
 };
