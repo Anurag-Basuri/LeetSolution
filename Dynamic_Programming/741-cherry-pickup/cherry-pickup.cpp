0ccc7c0749d4d8a558aaf2e1e044348ee69fa9fe
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-02-28 11:04:38 +0530

    Time: 40 ms (62.61%) | Memory: 41 MB (31.42%) - LeetSync

--- Dynamic_Programming/741-cherry-pickup/cherry-pickup.cpp
+++ Dynamic_Programming/741-cherry-pickup/cherry-pickup.cpp
@@ -1,39 +1,51 @@
 class Solution {
 public:
-    int f(int i1, int j1, int i2, vector<vector<int>>& grid,
-          vector<vector<vector<int>>> &dp, int n) {
-        int j2 = i1 + j1 - i2;
-
-        if (i1 >= n || j1 >= n || i2 >= n || j2 >= n)
-            return -1e9;
-
-        if (grid[i1][j1] == -1 || grid[i2][j2] == -1)
-            return -1e9;
-
-        if (i1 == n - 1 && j1 == n - 1)
-            return grid[i1][j1];
-
-        if (dp[i1][j1][i2] != -1)
-            return dp[i1][j1][i2];
-
-        int cherries =
-            (i1 == i2 && j1 == j2) ? grid[i1][j1] : grid[i1][j1] + grid[i2][j2];
-
-        int best = max({f(i1 + 1, j1, i2 + 1, grid, dp, n),
-                        f(i1 + 1, j1, i2, grid, dp, n),
-                        f(i1, j1 + 1, i2 + 1, grid, dp, n),
-                        f(i1, j1 + 1, i2, grid, dp, n)});
-
-        return dp[i1][j1][i2] = cherries + best;
-    }
-
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
+
         vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(n, vector<int>(n, -1)));
+            2*n - 1,
+            vector<vector<int>>(n, vector<int>(n, -1e9))
+        );
 
-        int ans = f(0, 0, 0, grid, dp, n);
+        dp[0][0][0] = grid[0][0];
 
-        return max(0, ans);
+        for(int k = 1; k < 2*n - 1; k++) {
+
+            for(int i1 = max(0, k - (n - 1));
+                i1 <= min(n - 1, k); i1++) {
+
+                for(int i2 = max(0, k - (n - 1));
+                    i2 <= min(n - 1, k); i2++) {
+
+                    int j1 = k - i1;
+                    int j2 = k - i2;
+
+                    if(j1 >= n || j2 >= n) continue;
+                    if(grid[i1][j1] == -1 || grid[i2][j2] == -1)
+                        continue;
+
+                    int best = -1e9;
+
+                    for(int p1 = i1; p1 >= i1 - 1; p1--) {
+                        for(int p2 = i2; p2 >= i2 - 1; p2--) {
+
+                            if(p1 >= 0 && p2 >= 0)
+                                best = max(best, dp[k-1][p1][p2]);
+                        }
+                    }
+
+                    if(best < 0) continue;
+
+                    int cherries = (i1 == i2)
+                        ? grid[i1][j1]
+                        : grid[i1][j1] + grid[i2][j2];
+
+                    dp[k][i1][i2] = best + cherries;
+                }
+            }
+        }
+
+        return max(0, dp[2*n - 2][n-1][n-1]);
     }
 };
