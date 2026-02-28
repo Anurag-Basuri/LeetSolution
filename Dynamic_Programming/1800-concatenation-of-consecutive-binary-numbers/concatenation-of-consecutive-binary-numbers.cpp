e3ecd2ee365e69383b636ac8f5f3d257e458482d
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-02-28 13:46:22 +0530

    Time: 40 ms (61.75%) | Memory: 7.8 MB (99.33%) - LeetSync

---
+++ Dynamic_Programming/1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int MOD = 1e9 + 7;
+        long long res = 0;
+        int bits = 0;
+
+        for (int i = 1; i <= n; i++) {
+            // if i is power of 2, increase bit length
+            if ((i & (i - 1)) == 0) {
+                bits++;
+            }
+
+            res = ((res << bits) % MOD + i) % MOD;
+        }
+
+        return res;
+    }
+};
