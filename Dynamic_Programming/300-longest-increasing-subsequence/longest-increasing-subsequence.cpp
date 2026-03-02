3400be39c328c59a77e7e54189ad41afba61849c
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-03-02 20:58:25 +0530

    Time: 1 ms (81.53%) | Memory: 14.4 MB (47.54%) - LeetSync

--- Dynamic_Programming/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ Dynamic_Programming/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+        vector<int> dp(n + 1, 1e9);
 
-        // dp[i][prevIndex + 1]
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        int len = 0;
 
-        for(int i = n - 1; i >= 0; i--) {
-            for(int prevIndex = i - 1; prevIndex >= -1; prevIndex--) {
+        for(int i = 0; i < n; i++) {
+            int low = 0;
+            int high = len; 
 
-                int notTake = dp[i + 1][prevIndex + 1];
+            while(low < high) {
+                int mid = low + (high - low) / 2;
 
-                int take = 0;
-                if(prevIndex == -1 || nums[i] > nums[prevIndex])
-                    take = 1 + dp[i + 1][i + 1];
-
-                dp[i][prevIndex + 1] = max(take, notTake);
+                if(dp[mid] >= nums[i]) high = mid;
+                else low = mid + 1;
             }
+
+            dp[low] = nums[i];
+
+            if(low == len) len++;
         }
 
-        return dp[0][0];
+        return len;
     }
 };
