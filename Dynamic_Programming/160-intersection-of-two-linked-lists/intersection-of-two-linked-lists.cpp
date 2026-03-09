5efd33c5e93b8487625595ed10c5cd1913119c3b
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-03-10 02:38:43 +0530

    Time: 58 ms (47.80%) | Memory: 23.9 MB (55.30%) - LeetSync

--- Dynamic_Programming/160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ Dynamic_Programming/160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,19 +8,24 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> store;
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* t1 = headA;
+        ListNode* t2 = headB;
 
-        while(headA != NULL) {
-            store.insert(headA);
-            headA = headA->next;
+        while (t1 != t2) {
+            t1 = t1->next;
+            t2 = t2->next;
+        
+            if (t1 == t2)
+                return t1;
+        
+            if (t1 == NULL)
+                t1 = headB;
+        
+            if (t2 == NULL)
+                t2 = headA;
         }
-
-        while(headB != NULL) {
-            if(store.find(headB) != store.end()) return headB;
-            headB = headB->next;
-        }
-
-        return NULL;
+        
+        return t1;
     }
 };
