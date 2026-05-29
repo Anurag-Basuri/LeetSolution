470b661760387c3a556f2fe639edf2276328448a
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-05-29 23:32:27 +0530

    Time: 9 ms (58.97%) | Memory: 29.1 MB (30.98%) - LeetSync

---
+++ Dynamic_Programming/2001-jump-game-vii/jump-game-vii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+
+        int reachable = 0;
+        for (int i = 1; i < n; i++) {
+            if (i - minJump >= 0)
+                reachable += dp[i - minJump];
+
+            if (i - maxJump - 1 >= 0)
+                reachable -= dp[i - maxJump - 1];
+
+            if (s[i] == '0' && reachable > 0)
+                dp[i] = 1;
+        }
+
+        return dp[n - 1];
+    }
+};
