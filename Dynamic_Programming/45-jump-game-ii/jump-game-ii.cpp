3c6472d2e668d2b020068ee4d845d7d23be2fef3
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-06-18 13:02:53 +0530

    Time: 3 ms (29.83%) | Memory: 20.7 MB (27.48%) - LeetSync

--- Dynamic_Programming/45-jump-game-ii/jump-game-ii.cpp
+++ Dynamic_Programming/45-jump-game-ii/jump-game-ii.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int recur(int idx, vector<int>& nums, vector<int>& dp, int n) {
-        if(idx >= n - 1)
-            return 0;
-
-        if(dp[idx])
-            return dp[idx];
-        
-        int ans = 1e7;
-        for(int i = 1; i <= nums[idx]; i++) {
-            if((i + idx) < n)
-                ans = min(ans, 1 + recur(i + idx, nums, dp, n));
-        }
-
-        return dp[idx] = ans;
-    }
-
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n + 1, 0);
+        int l = 0, r = 0;
+        int count = 0;
 
-        return recur(0, nums, dp, n);
+        while (r < n - 1) {
+            int farthest = 0;
+            
+            for (int i = l; i <= r; i++)
+                farthest = max(farthest, i + nums[i]);
+            
+            l = r + 1;
+            r = farthest;
+            count++;
+        }
+
+        return count;
     }
 };
