ca17f0e599d565d4968e8898b00607ed48fcfe4d
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-06-24 11:38:00 +0530

    Time: 43 ms (67.81%) | Memory: 13.2 MB (53.52%) - LeetSync

--- Dynamic_Programming/279-perfect-squares/perfect-squares.cpp
+++ Dynamic_Programming/279-perfect-squares/perfect-squares.cpp
@@ -3,23 +3,18 @@ public:
     int numSquares(int n) {
         int m = sqrt(n);
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 1e9));
-
-        for (int i = 0; i <= m; i++)
-            dp[i][0] = 0;
+        vector<int> dp(n + 1, 1e9);
+        dp[0] = 0;
 
         for (int i = 1; i <= m; i++) {
             int square = i * i;
 
             for (int target = 1; target <= n; target++) {
-                dp[i][target] = dp[i - 1][target];
-
                 if (target >= square)
-                    dp[i][target] =
-                        min(dp[i][target], 1 + dp[i][target - square]);
+                    dp[target] = min(dp[target], dp[target - square] + 1);
             }
         }
 
-        return dp[m][n];
+        return dp[n];
     }
 };
