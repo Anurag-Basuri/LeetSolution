f7ac9177211a7149818c5ff29ce9353f1c1d700b
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-06-25 07:33:42 +0530

    Time: 215 ms (34.18%) | Memory: 296.5 MB (19.59%) - LeetSync

--- Dynamic_Programming/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ Dynamic_Programming/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -6,7 +6,7 @@ public:
         int ans = 1;
 
         for(int i = 1; i < n + 1; i++) {
-            for(int j = 1; j < i + 1; j++) {
+            for(int j = 1; j < i; j++) {
                 dp[i][j] = dp[i][j - 1];
 
                 if(nums[i - 1] > nums[j - 1] && dp[j][j - 1] + 1 > dp[i][j])
