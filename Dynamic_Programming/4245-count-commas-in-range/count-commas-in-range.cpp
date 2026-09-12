574a78da39ab3360d44f255698f6abf9b3178bc6
Anurag Basuri <anuragbasuri@gmail.com>
2026-09-13 03:20:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.26%) - LeetSync

---
+++ Dynamic_Programming/4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int p = 1000, res = 0;
+        
+        while (p <= n) {
+            res += n - p + 1;
+            p *= 1000;
+        }
+
+        return res;
+    }
+};
