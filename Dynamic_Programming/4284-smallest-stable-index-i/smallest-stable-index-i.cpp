fbe9e72b22d4b37dec6f59acafcccbe6a4e7b976
Anurag Basuri <anuragbasuri@gmail.com>
2026-09-15 13:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (81.11%) - LeetSync

--- Dynamic_Programming/4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ Dynamic_Programming/4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        int sum = INT_MAX, n = nums.size(), ans = -1;
+        int sum = INT_MAX, n = nums.size();
         int min = nums[0];
         int max = nums[0];
 
@@ -9,12 +9,8 @@ public:
             if(nums[i] > max) max = nums[i];
             min = *min_element(nums.begin() + i, nums.end());
 
-            if(max - min < sum) {
-                sum = max - min;
-                ans = i;
-            }
-
-            if(sum <= k) return ans;
+            if(max - min <= k)
+                return i;
         }
 
         return -1;
