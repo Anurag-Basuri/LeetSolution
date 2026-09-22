d535641f14b764747613ee5cf4840c9a85c95919
Anurag Basuri <anuragbasuri@gmail.com>
2026-09-22 07:35:08 +0530

    Time: 777 ms (70.36%) | Memory: 18.7 MB (72.01%) - LeetSync

--- Dynamic_Programming/2108-minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.cpp
+++ Dynamic_Programming/2108-minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int f(int m, int target, int idx, int sum, vector<vector<int>> &mat, vector<vector<int>> &dp) {
-        if(idx < 0) return abs(target - sum);
-
-        if(dp[idx][sum] != -1) return dp[idx][sum];
-
-        int val = 1e9;
-
-        for(int i = 0; i < m; i++) {
-            val = min(val, f(m, target, idx - 1, sum + mat[idx][i], mat, dp));
-        }
-
-        return dp[idx][sum] = val;
-    }
-
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
         int n = mat.size();
         int m = mat[0].size();
-
-        int maxSum = 0;
-
+        
+        int sum = 0;
         for(int i = 0; i < n; i++)
-            maxSum += *max_element(mat[i].begin(), mat[i].end());
+            sum += *max_element(mat[i].begin(), mat[i].end());
+        
+        vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
+        dp[0][0] = true;
 
-        vector<vector<int>> dp(n, vector<int>(maxSum + 1, -1));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= sum; j++) {
+                if(!dp[i][j]) continue;
 
-        return f(m, target, n - 1, 0, mat, dp);
+                for(int k = 0; k < m; k++)
+                    dp[i + 1][j + mat[i][k]] = true;
+            }
+        }
+
+        int minVal = 1e9;
+        for(int i = 0; i <= sum; i++) {
+            if(dp[n][i])
+                minVal = min(minVal, abs(target - i));
+        }
+
+        return minVal;
     }
 };
